e9689e02730c942f6af9085ca513b7d7c7bf8258
Leahy <leahy@apache.org>
2017-03-29 15:45:51 -0700

    Updated model
    Updates to model that allow decoding of the base64 payload in the token.

--- src/jwt/jwtComponent.js
+++ src/jwt/jwtComponent.js
@@ -22,14 +22,17 @@
     function JwtController($log, JwtModel, JwtService){
         var self = this;
         self.jwtservice = new JwtService();
-        self.jwtmodel = new JwtModel();
+        self.jwtmodel = JwtModel;
+        $log.log(self.jwtmodel)
         self.login = function(email, password){
             self.jwtservice.token(email, password)
-                .then(function(d) {self.jwtmodel.token = d.token},function(d) {
-                self.ErrorMessage = "Sorry, we didn't recognize the email or password you entered. Please try again."
-            });  
-            $log.log(self.jwtmodel.header)
+            .then(
+                    function(d) {self.jwtmodel.hash = d.token;},
+                    function(d) {self.ErrorMessage = "Sorry, we didn't recognize the email or password you entered. Please try again."}
+            ).then(function(d) {$log.log(self.jwtmodel.body);})
+            .finally(function (df) {$log.log('end of promise');});;  
+            
+
         }
     }
-
 })();
--- src/jwt/jwtModel.js
+++ src/jwt/jwtModel.js
@@ -12,31 +12,34 @@
    */
 
     function JwtModel($log, base64){
-        var JwtModel = function(){
-            var self = this
-            self._token = '';
-            self._array = '';
-            self.header = {
-                get : function(){
-                   return self._token.toString().split('.', 1);}
-            };
-            self.body = {
-                get : function(){
-                    return base64.decode(self._array[1]);}
-            };
-            self.token = {
-                get : function(){return self._token},
-                set : function(value){
-                    self._token = value;
-                    self._array = value.split('.', 2);
-                }
-            };
 
-            // Call the initialize function for every new instance
-            self.init = function () {};
-            return self.init
-        }
-        return JwtModel;
+//            self.header = {
+//                get : function(){
+//                   return self._token.split('.', 1);}
+//            };
+//            self.body = {
+//                get : function(){
+//                    return base64.decode(self._array[1]);}
+//            };
+//            self.token = {
+//                get : function(){return self._token},
+//                set : function(value){
+//                    self._token = value;
+//                    self._array = value.split('.', 2);
+//                }
+//            };
+            var JwtModel = {
+                _token : '',
+                _array : '',
+                get hash(){return this._token},
+                set hash(value){
+                    this._token = value;
+                    this._array = value.split('.');
+                },
+                get header(){return base64.decode(this._array[0]);},
+                get body(){return base64.decode(this._array[1]);}
+            };
+            return JwtModel
     }
     
 })();
