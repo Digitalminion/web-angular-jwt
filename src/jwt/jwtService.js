df50c19827b05793aa2998849e6b69167305d747
Leahy <leahy@apache.org>
2017-03-30 02:11:57 -0700

    Globals

--- src/jwt/jwtService.js
+++ src/jwt/jwtService.js
@@ -10,9 +10,12 @@
    * @returns {{jwtAuthService: object}
    * @constructor
    */
+  function JwtService(apiDomain,apiAuthTokenUrl,apiAuthTokenRefreshUrl,$http, $q, $log){
       var JwtService = function(){
+        var self = this;
         self.token = function(email, pass) {
             var promise = $http.post(apiDomain + apiAuthTokenUrl, {email: email, password: pass}).then(function (response) {
+                return response.data;  
             });
             return promise;
         };
@@ -27,4 +30,3 @@
       }
       return JwtService;
   }
-
