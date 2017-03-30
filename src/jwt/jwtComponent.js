3713c2d9a0e19873a0da5702c4a00e5c67400325
Leahy <leahy@apache.org>
2017-03-30 03:34:03 -0700

    Expanded service to check token ttl

--- src/jwt/jwtComponent.js
+++ src/jwt/jwtComponent.js
@@ -6,21 +6,33 @@
                 bindings: {email: '<', password: '<'},
                 controller: JwtController,
                 controllerAs: 'auth',
-                template:'<div> <alert> <strong>{{ auth.ErrorMessage }}</strong></alert></div><div><button ng-click="auth.login(auth.email,auth.password)">Sign in</button></div>'
+                template:'<div> <alert> <strong>{{ auth.Message }}</strong></alert></div><div><button ng-click="auth.login(auth.email,auth.password)">Sign in</button></div><div><button ng-click="auth.check()">Check Session</button></div>'
                 });
 
     function JwtController($log, JwtModel, JwtService){
         var self = this;
         self.jwtservice = new JwtService();
-        self.jwtmodel = JwtModel;
-        self.login = function(email, password){
-            self.jwtservice.token(email, password)
-            .then(
-                    function(d) {self.jwtmodel.hash = d.token;
-                                self.loggedIn = true;},
-                    function(d) {self.ErrorMessage = "Sorry, we didn't recognize the email or password you entered. Please try again."}
-            )
-            .finally(function (df) {$log.log('end of promise');});  
+        self.login = function(email, password){ 
+            self.jwtservice.login({email: email, pass: password})
+                .then(
+                        function(token) {
+                                    self.jwtservice.values = token;
+                                    self.Message = "You have saved a token!"
+                                    return token;},
+                        function(token) {self.Message = "Sorry, we didn't recognize the email or password you entered. Please try again."}
+                )
+                .then(function (token) {$log.log('end of promise');})  
+        }
+        
+        self.check = function(){ 
+            self.jwtservice.token.then(
+                    function(token) {
+                                self.jwtservice.values = token;
+                                self.Message = "Your token still works"
+                                return token;},
+                    function(token) {
+                        self.Message = "Sorry, the session has expired"}
+            );  
         }
     }
 })();
--- src/jwt/jwtService.js
+++ src/jwt/jwtService.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
   angular.module('authJwt')
-         .service('JwtService', ['apiDomain','apiAuthTokenUrl','apiAuthTokenRefreshUrl','$http', '$q', '$log', JwtService]);
+         .service('JwtService', ['apiDomain','apiAuthTokenUrl','apiAuthTokenRefreshUrl','$http', '$q', '$log','base64', JwtService]);
 
   /**
    * JWT Request Service
@@ -10,23 +10,97 @@
    * @returns {{jwtAuthService: object}
    * @constructor
    */
-  function JwtService(apiDomain,apiAuthTokenUrl,apiAuthTokenRefreshUrl,$http, $q, $log){
-      var JwtService = function(){
-        var self = this;
-        self.token = function(email, pass) {
-            var promise = $http.post(apiDomain + apiAuthTokenUrl, {email: email, password: pass}).then(function (response) {
-                return response.data;  
-            });
-            return promise;
-        };
-        self.token_refresh = function(token) {
-            var promise = $http.post(apiDomain + apiAuthTokenRefreshUrl, {token: token}).then(function (response) {
-                return response.data;
-            });
-            return promise;
-        };
-        self.init = function(){};
-    	return self.init();
+//  function JwtService(apiDomain,apiAuthTokenUrl,apiAuthTokenRefreshUrl,$http, $q, $log){
+//      var JwtService = function(){
+//        var self = this;
+//        self.token = function(email, pass) {
+//            var promise = $http.post(apiDomain + apiAuthTokenUrl, {email: email, password: pass}).then(function (response) {
+//                return response.data;  
+//            });
+//            return promise;
+//        };
+//        self.token_refresh = function(token) {
+//            var promise = $http.post(apiDomain + apiAuthTokenRefreshUrl, {token: token}).then(function (response) {
+//                return response.data;
+//            });
+//            return promise;
+//        };
+//        self.init = function(){};
+//    	return self.init();
+//      }
+//      return JwtService;
+//  }
+    
+      
+    function JwtService(apiDomain,apiAuthTokenUrl,apiAuthTokenRefreshUrl,$http, $q, $log, base64){
+      var JwtService = class{
+            constructor(){
+                this._token = null;
+                this._array = [];
+                this.header = {};
+                this.body = {};
+            }
+
+            login(login){
+                //    Create a deferred operation.
+               var deferred = $q.defer();
+                    //    Get the token from the server.
+                    $http.post(apiDomain + apiAuthTokenUrl, {email: login.email, password: login.pass})
+                    .then(function(response) {
+                        deferred.resolve(response.data.token);
+                    }, function(response) {
+                        deferred.reject(response);
+                    });
+                //    Now return the promise.
+                return deferred.promise;
+            }
+
+            get token(){
+                //    Create a deferred operation.
+               var deferred = $q.defer();
+                //    Make sure we already have the token, if it has time left we can resolve the promise.
+                if(this._token !== null) {
+                    $log.log(this.ttl)
+                    if(this.ttl == 'valid') {
+                        deferred.resolve(this._token);
+                    }
+                    else if (this.ttl == 'renew'){
+                        //    Get the name from the server.
+                        $http.post(apiDomain + apiAuthTokenRefreshUrl, {token: _token})
+                        .then(function(response) {
+                            deferred.resolve(response.data.token);
+                        }, function(response) {
+                            deferred.reject(response);
+                        });
+                    }else {
+                        deferred.reject('expired');
+                    }   
+                } else {
+                        deferred.reject('expired');
+                }
+                //    Now return the promise.
+                return deferred.promise;
+            }
+
+            get ttl(){
+                var current = ((new Date).getTime())/1000; 
+                if ((current-this.body.exp) > 600){
+                    return 'expired'
+                }else if((current-this.body.exp) > 450){
+                    return 'renew'    
+                }
+                else if((current-this.body.exp) < 450){
+                    return 'valid'
+                }else{return 'expired'}
+            }
+            set values(token){
+                this._token = token;
+                this._array = token.split('.');
+                this.header = JSON.parse(base64.decode(this._array[0]));
+                this.body = JSON.parse(base64.decode(this._array[1]).replace(/[\u0000]/g, ''));  
+            }
       }
       return JwtService;
   }
+
+})();
